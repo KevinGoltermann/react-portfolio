2a874c3b6464dd5cb4bf41c82c8289f940caf77a
Kevin Goltermann <kggoltermann@gmail.com>
2021-10-31 23:37:10 -0600

    Added all nav components and tested to make sure they are working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import Navbar from "./components/navbar/Navbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/works/Works";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+
 
 
 function App() {
   return (
     <div className="app">
-      hello my name is kevin
+      <Navbar/>
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Works/>
+        <Testimonials/>
+        <Contact/>
+      </div>
     </div>
   );
 }
