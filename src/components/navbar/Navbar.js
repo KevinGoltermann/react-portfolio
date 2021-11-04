4980132927b23b596ffc08296a1ab74caf4f1386
Kevin Goltermann <kggoltermann@gmail.com>
2021-11-04 15:22:28 -0600

    Added material-ui and used phone and email icons in navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,6 @@
 import "./navbar.scss"
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 export default function Navbar() {
 	return (
@@ -6,6 +8,14 @@ export default function Navbar() {
 			<div className="wrapper">
 				<div className="left">
 					<a href="#intro" className="logo">KG</a>
+					<div className="itemcontainer">
+						<PhoneIphoneIcon className="icon"/>
+						<span>630-981-7434</span>
+					</div>
+					<div className="itemcontainer">
+						<MailOutlineIcon className="icon"/>
+						<span>kggoltermann@gmail.com</span>
+					</div>					
 				</div>
 				<div className="right">
 				</div>
