8f50c420b8c0101a11eaf429fcf9a466cf77807a
Kevin Goltermann <kggoltermann@gmail.com>
2021-11-05 17:20:25 -0600

    Navbar transition color and hamburger is animated

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,12 +2,12 @@ import "./navbar.scss"
 import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
-export default function Navbar() {
+export default function Navbar({ menuOpen, setMenuOpen }) {
 	return (
-		<div className="navbar">
+		<div className={"navbar " + (menuOpen && "active")}>
 			<div className="wrapper">
 				<div className="left">
-					<a href="#intro" className="logo">KG</a>
+					<a href="#intro" className="logo" style={{ color: 'inherit' }}>KG</a>
 					<div className="itemcontainer">
 						<PhoneIphoneIcon className="icon"/>
 						<span>630-981-7434</span>
@@ -18,6 +18,11 @@ export default function Navbar() {
 					</div>					
 				</div>
 				<div className="right">
+					<div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+						<span className="line1"></span>
+						<span className="line2"></span>
+						<span className="line3"></span>
+					</div>
 				</div>
 			</div>
 		</div>
