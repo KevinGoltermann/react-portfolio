0fdf0c8e252bb029740a7a75b023d300baa3a131
Kevin Goltermann <kggoltermann@gmail.com>
2021-11-05 19:17:30 -0600

    Sidebar menu working with hamburger menu

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu"
 import "./app.scss"
 import { useState } from 'react';
 
@@ -13,7 +14,8 @@ function App() {
   const [menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="app">
-      <Navbar menuOpen={ menuOpen} setMenuOpen={setMenuOpen}/>
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
