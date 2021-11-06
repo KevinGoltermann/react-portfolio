abcf8b6eb5af8c3c485b93a90a8bbd6396d78b83
Kevin Goltermann <kggoltermann@gmail.com>
2021-11-06 01:11:23 -0600

    Intro page finished with avatar and portfolio icon

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,8 +1,22 @@
 import "./intro.scss"
+import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
+
 export default function Intro() {
 	return (
 		<div  className="intro" id="intro">
-			Intro
+			<div className="left">
+				<div className="imgContainer">
+					<img src="assets/avatar.svg" alt=""/>
+				</div>
+			</div>
+			<div className="right">
+				<div className="wrapper">
+					<h2>Welcome!</h2>
+					<h1>// My name is Kevin //</h1>
+					<h3><span>Placeholder</span></h3>
+				</div>
+				<a href="#portfolio"><ArrowCircleDownIcon/></a>
+			</div>
 		</div>
 	)
 }
