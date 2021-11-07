ccb31694f2cf4976346ff6cda3f66d34fa317acb
Kevin Goltermann <kggoltermann@gmail.com>
2021-11-07 15:22:44 -0700

    implemented cursor js and finished intro section

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,7 +1,22 @@
 import "./intro.scss"
 import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
+import { init } from 'ityped';
+import { useEffect, useRef} from 'react';
 
 export default function Intro() {
+
+	const textRef = useRef();
+
+	useEffect(() => {
+		init(textRef.current, { 
+			showCursor: true, 
+			startDelay: 1000,
+			backDelay:  1000,
+			backSpeed:  70,
+			strings: ['Front End Developer', 'Photographer', 'Learner' ] 
+		});
+	}, [])
+
 	return (
 		<div  className="intro" id="intro">
 			<div className="left">
@@ -13,7 +28,7 @@ export default function Intro() {
 				<div className="wrapper">
 					<h2>Welcome!</h2>
 					<h1>// My name is Kevin //</h1>
-					<h3><span>Placeholder</span></h3>
+					<h3><span ref={textRef}></span></h3>
 				</div>
 				<a href="#portfolio"><ArrowCircleDownIcon/></a>
 			</div>
