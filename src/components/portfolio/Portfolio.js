6fd28e52f581247d1b3ab0401e570aa311e62f8f
Kevin Goltermann <kggoltermann@gmail.com>
2021-11-11 18:45:13 -0700

    Finished portfolio section with sortable portfolio links

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,11 +1,12 @@
 import "./portfolio.scss"
 import PortfolioList from "../portfolioList/PortfolioList"
-import { useState } from 'react';
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { featuredPortfolio, webPortfolio, mobilePortfolio, designPortfolio, contentPortfolio } from '../../data'
 
 export default function Portfolio() {
-
-	const [selected, setSelected] = useState("featured")
-
+	const [selected, setSelected] = useState("featured");
+	const [data, setData] = useState([]);
 	const list = [
 		{
 			id: "featured",
@@ -29,6 +30,28 @@ export default function Portfolio() {
 		},
 	];
 
+	useEffect(()=>{
+		switch(selected){
+			case "featured":
+				setData(featuredPortfolio)
+				break;
+			case "web":
+				setData(webPortfolio)
+				break;
+			case "mobile":
+				setData(mobilePortfolio)
+				break;
+			case "design":
+				setData(designPortfolio)
+				break;
+			case "content":
+				setData(contentPortfolio)
+				break;
+			default:
+				setData(featuredPortfolio);
+		}
+	}, [selected])
+
 	return (
 		<div className="portfolio" id="portfolio">
 			<h1>Portfolio</h1>
@@ -41,30 +64,14 @@ export default function Portfolio() {
 				))}
 			</ul>
 			<div className="container">
+				{data.map(d=>(
+
 				<div className="item">
-					<img src="https://via.placeholder.com/200" alt=""/>
-					<h3>Banking App</h3>
+					<img src={d.img} alt=""/>
+					<h3>{d.title}</h3>
 				</div>
-				<div className="item">
-					<img src="https://via.placeholder.com/200" alt=""/>
-					<h3>Banking App</h3>
-				</div>
-				<div className="item">
-					<img src="https://via.placeholder.com/200" alt=""/>
-					<h3>Banking App</h3>
-				</div>
-				<div className="item">
-					<img src="https://via.placeholder.com/200" alt=""/>
-					<h3>Banking App</h3>
-				</div>
-				<div className="item">
-					<img src="https://via.placeholder.com/200" alt=""/>
-					<h3>Banking App</h3>
-				</div>
-				<div className="item">
-					<img src="https://via.placeholder.com/200" alt=""/>
-					<h3>Banking App</h3>
-				</div>
+
+				))}
 			</div>
 		</div>
 	)
